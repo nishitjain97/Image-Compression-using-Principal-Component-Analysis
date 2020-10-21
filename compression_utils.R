137477e044c6ab9e782e8a897bf9bfbdc57691a1
Nishit.Jain <nishit.jain@mu-sigma.com>
2020-10-21 11:45:34 +0530

    feat: This commit will update plotImage function.
    The function now plots multiple raster images given in the form of a list.

--- compression_utils.R
+++ compression_utils.R
@@ -1,5 +1,40 @@
-plotImage <- function(rasterObject) {
-  par(pty="s")
-  plot(NA, xlim=c(0,nrow(rasterObject)),ylim=c(0,ncol(rasterObject)), xlab="", ylab="")
-  rasterImage(rasterObject, 0, 0, nrow(rasterObject), ncol(rasterObject))
+plotImage <- function(rasterObjects) {
+  if(length(rasterObjects) == 1) {
+    par(pty="s")
+  }
+  
+  xlimits <- 0
+  ylimits <- ncol(rasterObjects[[1]])
+  
+  for(i in 1:length(rasterObjects)) {
+    xlimits <- xlimits + nrow(rasterObjects[[i]])
+    xlimits <- xlimits + 0.5
+    
+    if(ylimits < ncol(rasterObjects[[i]])) {
+      ylimits <- ncol(rasterObjects[[i]])
+    }
+  }
+  
+  plot(NA, xlim = c(0, xlimits), ylim = c(0, ylimits), xlab="", ylab="")
+  
+  xleft <- 0
+  ybottom <- 0
+  xright <- 0
+  ytop <- 0
+  
+  
+  for(i in 1:length(rasterObjects)) {
+    if(i == 1) {
+      xleft <- 0
+      xright <- nrow(rasterObjects[[i]])
+    } else {
+      xleft <- xleft + nrow(rasterObjects[[i-1]]) + 0.5
+      xright <- xright + nrow(rasterObjects[[i]]) + 0.5
+    }
+    
+    ybottom <- 0
+    ytop <- ncol(rasterObjects[[i]])
+    
+    rasterImage(rasterObjects[[i]], xleft, ybottom, xright, ytop)
+  }
 }
